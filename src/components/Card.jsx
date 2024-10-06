a0902c7405cc27c926e1b251514d53ef6a876dd4
Nandana <nandanasheri@gmail.com>
2024-10-05 22:32:02 -0500

    tweaking styles + starting card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import styles from './Card.module.css'
+
+// This is our reusable card component
+const Card = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Card;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,13 @@
+// CSS Modules localize CSS so that I can reuse class names and not have my styles interact
+// with each other
+import styles from './LandingPage.module.css'
 
+// Each React component is essentially a function that returns a bunch of HTML so
+// it's important that we wrap it all within a div
 export default function LandingPage() {
     return (
-        <>
-        hi !
-        </>
+        <div className={styles.layout}>
+            <h1>welcome to wics dev project !</h1>
+        </div>
     )
 }
